45d70ad74f66f2c094c4530939c5fe44b95492e4
Green Kawinphop <25kawinphop@gmail.com>
2023-10-15 11:32:40 +0700

    layout.html

--- Green/mysite/blogs/views.py
+++ Green/mysite/blogs/views.py
@@ -2,12 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from category.models import Category
 from .models import Blogs
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
     blogs = Blogs.objects.all()
+    # if .order_by('pk') = 1,2,3,4
+    # '-pk' = 4,3,2,1
+    # [:2] แสดง 2 บทความล่าสุด
+    latest = Blogs.objects.all().order_by('-pk')[:2]
+
+    # Pagination
+    paginator = Paginator(blogs,4)
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+    
+    try:
+        blogPerpage = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        blogPerpage = paginator.page(paginator.num_pages)
+
     return render(request, "frontend/index.html", {
         'categories': categories,
-        'blogs': blogs 
+        'blogs': blogPerpage,
+        'latest': latest,
         })
+
+def blogDetail(request, id):
+    singleBlog = Blogs.objects.get(id = id)
+    return render(request, "frontend/blogDetail.html", {"blog":singleBlog})
