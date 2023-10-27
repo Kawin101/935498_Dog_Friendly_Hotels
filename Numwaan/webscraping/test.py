4566bff05ea2c6e1ae68b276f9f3d82da0ff8f5e
Thidaratmatha05 <76549405+Thidaratmatha05@users.noreply.github.com>
2023-10-27 12:21:58 +0700

    up

--- Numwaan/webscraping/test.py
+++ Numwaan/webscraping/test.py
@@ -15,13 +15,16 @@ for tags in soup.find_all('h3'):
 
 
 
+url = 'https://travel.trueid.net/detail/rDvYykOdorNR'
+page = urllib.request.urlopen(url)
 
- 
-# Can be a list of tags
-# tags = ['h1', 'h2', 'h3']
-# for tags in soup.find_all(tags):
-# for tags in soup.find_all('h3'):
-    # print(tags.text.encode('utf-8'))
+soup = BeautifulSoup(page, 'html.parser')
 
-# a = 'xe0\xb8\x9a\xe0\xb8\xb2\xe0\xb8\x87\xe0\xb9\x81\xe0\xb8\xaa\xe0\xb8\x99 \xe0\xb8\x8a\xe0\xb8\xa5\xe0\xb8\x9a\xe0\xb8\xb8\xe0\xb8\xa3\xe0\xb8\xb5'
-# print(a.encoding('utf-8'))
+# con = soup.find_all("h3")
+# print(con.encode('utf-8'))
+
+for tags in soup.find_all('h3'):
+    print(tags.text)
+
+for ta in soup.find_all('ul'):
+    print(ta.text)
