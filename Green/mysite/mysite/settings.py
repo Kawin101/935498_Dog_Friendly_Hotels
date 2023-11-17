3dce06c65f95112cfa7ea559c83b7bacd72ebc64
Kawinphop (Green) Chomnikorn <25kawinphop@gmail.com>
2023-11-17 15:17:30 +0700

    up

--- Green/mysite/mysite/settings.py
+++ Green/mysite/mysite/settings.py
@@ -89,7 +89,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -107,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
